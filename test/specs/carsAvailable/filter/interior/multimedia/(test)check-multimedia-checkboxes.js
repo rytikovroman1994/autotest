a202ec0853edd610804d2dd9bcc698807f8171be
Kodix <Kodix>
2018-09-11 19:30:31 +0300

    Пробный тест для проверки всех чекбоксов на странице мультимедиа

---
+++ test/specs/carsAvailable/filter/interior/multimedia/(test)check-multimedia-checkboxes.js
@@ -0,0 +1,79 @@
+const imageName = [
+    'activeInfoDisplay',
+    'appConnect',
+    'bluetooth',
+    'navigation',
+    'premiumAudio',
+    'wirelessСharger'
+];
+
+const optionName = [
+    'ACTIVE INFO DISPLAY',
+    'APP CONNECT',
+    'BLUETOOTH',
+    'НАВИГАЦИЯ',
+    'ПРЕМИУМ АУДИО',
+    'QI ЗАРЯДКА'
+];
+
+describe('check of multimedia checkboxes', () => {
+    const ctx = {
+        originalScreenshot: null,
+        newScreenshot: null,
+      };
+    before('open page multimedia', () => {
+        browser.helpers.openSite();
+        // переходим на страницу интерьер
+        browser.click('#react-tabs-8');
+        // ожидаем появление картинки кресла
+        browser.waitForVisible('.avn008_image-switcher_image');
+        // переходим на страницу мультимедиа 
+        browser.click('#react-tabs-18');
+        // ожидаем загрузки картинки диагональ экрана
+        browser.waitForVisible('.multimedia img');
+    });
+
+    // проверяем каждый чекбокс по отдельности
+        for( let i = 1; i <= 6; i++ ) {
+            const name = imageName[i - 1];
+            it(`test multimedia checkbox ${name}`, () => {
+                // проверяем что фильтр пуст
+                browser.waitUntil(
+                    ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
+                    5000, "На странице уже есть одно условие фильтра");
+                // включаем чекбокс
+                browser.click(`div:nth-child(${i}) > div > div > div.avn008_option-check_checkbox-self > label`);
+                // проверяем, что в фильтре появилось условие
+                browser.waitForExist('.avn008_filter-value-item_image');
+                const topicalText = browser.getText('.avn008_filter-value-item_text__bottom');
+                const verifiableText = optionName[i - 1];
+                expect(topicalText).to.be.equal(verifiableText);
+                // убираем условие
+                browser.click(`div:nth-child(${i}) > div > div > div.avn008_option-check_checkbox-self > label`);
+                // проверяем, что условие пропало
+                browser.waitUntil(
+                    ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
+                    5000, "На странице уже есть одно условие фильтра");
+
+
+                // открываем всплывающее окно
+                browser.click(`div:nth-child(${i}) > div > div > div.avn008_option-check_more`);
+                // ждём появления картинки
+                browser.waitForVisible('div.avn015_image > div > div.LazyLoad.is-visible > div > img');
+                // берём скриншот с локала
+                ctx.originalScreenshot = `snapshot/screenshotInterior/${name}.png`;
+                // делаем актуальный скриншот
+                ctx.newScreenshot = browser.screenshot().value;
+                // закрываем всплывающее окно
+                browser.click('.modal-window_close');
+
+                it('compare screenshots', async () => {
+                    expect(ctx.originalScreenshot).not.equal(null);
+                    expect(ctx.newScreenshot).not.equal(null);
+                
+                    const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+                    expect(distance).to.be.below(0.1);
+                });
+            });
+        }
+});
