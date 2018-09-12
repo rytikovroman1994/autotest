4ddf656703e0bbdba5b4855442759583fde3f104
Kodix <Kodix>
2018-09-12 13:23:10 +0300

    Тест чекбокса и подробнее AID

---
+++ test/specs/carsAvailable/filter/interior/multimedia/active-info-display.js
@@ -0,0 +1,54 @@
+describe('test active info display', () => {
+    const ctx = {
+        originalScreenshot: null,
+        newScreenshot: null,
+      };
+    before(' open page multimedia', () => {
+        browser.helpers.openSite();
+        // открываем страницу экстерьер
+        browser.click('#react-tabs-8');
+        // ожидаем появление картинки кресла
+        browser.waitForVisible('.avn008_image-switcher_image');
+        // открываем страницу мульдимедиа
+        browser.click('#react-tabs-18');
+    });
+
+     it('check checkbox dynamic active info display', () => {
+        // проверяем что фильтр пуст
+        browser.waitUntil(
+            ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
+        5000, "На странице уже есть одно условие фильтра");
+        // включаем чекбокс
+        browser.click('div:nth-child(1) > div > div > div.avn008_option-check_checkbox-self > label');
+        // проверяем, что в фильтре появилось условие
+        browser.waitForExist('.avn008_filter-value-item_image');
+        // проверяем, что это именно фаркоп
+        const text = browser.getText('.avn008_filter-value-item_text__bottom');
+        expect(text).to.be.equal('ACTIVE INFO DISPLAY');
+        // убираем условие
+        browser.click('div:nth-child(1) > div > div > div.avn008_option-check_checkbox-self > label');
+        // проверяем, что условие пропало
+        browser.waitUntil(
+            ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
+        5000, "На странице уже есть одно условие фильтра");
+     });
+
+     it('check more in detail about active info display', () => {
+        // открываем всплывающее окно
+        browser.click('div:nth-child(1) > div > div > div.avn008_option-check_more');
+        // ждём появления картинки
+        browser.waitForVisible('div.avn015_image > div > div.LazyLoad.is-visible > div > img');
+        // берём скриншот с локала
+        ctx.originalScreenshot = 'snapshot/screenshotInterior/activeInfoDisplay.png';
+        // делаем актуальный скриншот
+        ctx.newScreenshot = browser.screenshot().value;
+     });
+
+     it('compare screenshots', async () => {
+        expect(ctx.originalScreenshot).not.equal(null);
+        expect(ctx.newScreenshot).not.equal(null);
+    
+        const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+        expect(distance).to.be.below(0.1);
+      });
+});
