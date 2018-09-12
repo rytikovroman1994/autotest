4e534b9ae571c75f6d869daa8a911887dcf20d3b
Kodix <Kodix>
2018-09-12 20:19:56 +0300

    Тест страницы безопасность скриншотом

---
+++ test/specs/carsAvailable/filter/options/security/screenshot-security.js
@@ -0,0 +1,36 @@
+// тест скриншотом страницы безопасность
+describe('screenshot page security', () => {
+    const ctx = {
+        originalScreenshot: null,
+        newScreenshot: null,
+      };
+    before(() => {
+        browser.helpers.openSite();
+        // открываем страницу опции
+        browser.click('#react-tabs-10');
+        // переходим на страницу безопасность
+        browser.click('#react-tabs-14');
+        // ожидаем появление картинки 
+        browser.waitForVisible('.avn008_safety-images_main .LazyLoad.is-visible  img');
+    });
+
+    it('take screenshot page comfort', () => {
+        // берём скриншот с локала
+        ctx.originalScreenshot = 'snapshot/screenshotOption/security.png';
+    });
+
+    it('do second screenshot', () => {
+        // делаем текущий скриншот
+        ctx.newScreenshot = browser.screenshot().value;
+    });
+
+    it('compare screenshots', async () => {
+        expect(ctx.originalScreenshot).not.equal(null);
+        expect(ctx.newScreenshot).not.equal(null);
+    
+        const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+    
+        // expect(distance).to.be.above(0);
+        expect(distance).to.be.below(0.1);
+      });
+});
