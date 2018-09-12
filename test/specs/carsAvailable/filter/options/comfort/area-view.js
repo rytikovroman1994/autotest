0d78053bd799f784304d2ab2434d28999ffb01ec
Kodix <Kodix>
2018-09-12 13:58:07 +0300

    Тест чексбокса и подробнее area view

---
+++ test/specs/carsAvailable/filter/options/comfort/area-view.js
@@ -0,0 +1,52 @@
+describe('test area view', () => {
+    const ctx = {
+        originalScreenshot: null,
+        newScreenshot: null,
+      };
+    before(' open page options', () => {
+        browser.helpers.openSite();
+        // открываем страницу опции
+        browser.click('#react-tabs-10');
+        // ожидаем появление последней картинки
+        browser.waitForVisible('div:nth-child(7) .avn008_option-check_image img');
+    });
+
+     it('check checkbox dynamic area view', () => {
+        // проверяем что фильтр пуст
+        browser.waitUntil(
+            ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
+        5000, "На странице уже есть одно условие фильтра");
+        // включаем чекбокс
+        browser.click('div:nth-child(1) > div > div > div.avn008_option-check_checkbox-self > label');
+        // проверяем, что в фильтре появилось условие
+        browser.waitForExist('.avn008_filter-value-item_image');
+        // проверяем, что это именно фаркоп
+        const text = browser.getText('.avn008_filter-value-item_text__bottom');
+        expect(text).to.be.equal('СИСТЕМА AREA VIEW');
+        // убираем условие
+        browser.click('div:nth-child(1) > div > div > div.avn008_option-check_checkbox-self > label');
+        // проверяем, что условие пропало
+        browser.waitUntil(
+            ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
+        5000, "На странице уже есть одно условие фильтра");
+     });
+
+     it('check more in detail about area view', () => {
+        // открываем всплывающее окно
+        browser.click('div:nth-child(1) > div > div > div.avn008_option-check_more');
+        // ждём появления картинки
+        browser.waitForVisible('div.avn015_image > div > div.LazyLoad.is-visible > div > img');
+        // берём скриншот с локала
+        ctx.originalScreenshot = 'snapshot/screenshotOption/areaView.png';
+        // делаем актуальный скриншот
+        ctx.newScreenshot = browser.screenshot().value;
+     });
+
+     it('compare screenshots', async () => {
+        expect(ctx.originalScreenshot).not.equal(null);
+        expect(ctx.newScreenshot).not.equal(null);
+    
+        const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+        expect(distance).to.be.below(0.1);
+      });
+});
