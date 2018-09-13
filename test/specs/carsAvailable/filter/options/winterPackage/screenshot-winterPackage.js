f0ed7e0b194f7f40c7a81b7dbc2e5c58e3941af0
Kodix <Kodix>
2018-09-13 13:34:54 +0300

    Тест скриншотом страницы зимний пакет

---
+++ test/specs/carsAvailable/filter/options/winterPackage/screenshot-winterPackage.js
@@ -0,0 +1,36 @@
+// тест скриншотом страницы мультимедиа
+describe('screenshot page winter package', () => {
+    const ctx = {
+        originalScreenshot: null,
+        newScreenshot: null,
+      };
+    before(() => {
+        browser.helpers.openSite();
+        // переходим на страницу опции
+        browser.click('#react-tabs-10');
+        // переходим на страницу асисистенты 
+        browser.click('#react-tabs-18');
+        // ожидаем загрузки картинки асистенты парковки
+        browser.waitForVisible('.avn008_option__slider-card-icon');
+    });
+
+    it('take screenshot page winter package', () => {
+        // берём скриншот с локала
+        ctx.originalScreenshot = 'snapshot/screenshotOption/winterPackage.png';
+    });
+
+    it('do second screenshot', () => {
+        // делаем текущий скриншот
+        ctx.newScreenshot = browser.screenshot().value;
+    });
+
+    it('compare screenshots', async () => {
+        expect(ctx.originalScreenshot).not.equal(null);
+        expect(ctx.newScreenshot).not.equal(null);
+    
+        const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+    
+        // expect(distance).to.be.above(0);
+        expect(distance).to.be.below(0.1);
+      });
+});
