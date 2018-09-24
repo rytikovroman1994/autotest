b88de5146430aed90df8c3c7e717eda708d1d3f8
Kodix <Kodix>
2018-09-24 18:48:08 +0300

    Правки классов

--- test/specs/carsAvailable/filter/exterior/options/railings.js
+++ test/specs/carsAvailable/filter/exterior/options/railings.js
@@ -35,7 +35,7 @@ describe('test dynamic railings', () => {
         // открываем всплывающее окно
         browser.click('div:nth-child(3) > div > div > div.avn008_option-check_more');
         // ждём появления картинки
-        browser.waitForVisible('.avn015_image picture img');
+        browser.waitForVisible('.avn015_content .image-container');
         // берём скриншот с локала
         ctx.originalScreenshot = 'snapshot/screenshotExterior/railings.png';
         // делаем актуальный скриншот
