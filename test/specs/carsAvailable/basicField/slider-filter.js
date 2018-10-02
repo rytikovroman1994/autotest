75255e43943b38c193bca5c48587bfcbddd14e8e
Kodix <Kodix>
2018-10-02 16:18:52 +0300

    Тест на проверку работы слайдера и отображаения картинок в карточках слайдреа

---
+++ test/specs/carsAvailable/basicField/slider-filter.js
@@ -0,0 +1,27 @@
+describe('test slider filter', () => {
+    before('open page filter', () => {
+        browser.helpers.openSite();
+    });
+
+    // проверяем работу слайдера вправо
+    it('click right slider button', () => {
+    while (browser.isVisible('.slick-next.slick-disabled') == false) {  
+        browser.click('.slick-next');
+        // проверяем, что нет оверлея на картинке
+        browser.waitUntil(
+            () => browser.isVisible('.image-lazyload_overlay.is_visible .content-loader__self') === false,
+            5000, "Вместо картинки появился оверлей");
+        }
+    // проверяем, что появился класс, заблокированной кнопки
+        browser.waitForExist('.slick-next.slick-disabled');
+    });
+
+    // проверяем работу слайдера влево
+    it('click right slider button', () => {
+        while (browser.isVisible('.slick-prev.slick-disabled') == false) {  
+            browser.click('.slick-prev');
+            }
+        // проверяем, что появился класс, заблокированной кнопки
+            browser.waitForExist('.slick-prev.slick-disabled');
+        });
+    });
