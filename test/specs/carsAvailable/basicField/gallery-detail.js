33a63d884f6ac6a34237e92f1489f800519eb137
Kodix <Kodix>
2018-10-03 13:46:59 +0300

    Тест на проверку наличия картинок в галерее

---
+++ test/specs/carsAvailable/basicField/gallery-detail.js
@@ -0,0 +1,50 @@
+describe('test gallery in detail', () => {
+
+    // пусть к кнопке
+    let wayButton = '.avn007-3_display-options div:nth-child(1) >.avn007-3_item-text';
+
+    before('open page detail', () => {
+        browser.helpers.openList();
+        // отркываем деталку
+        browser.leftClick('.avn001-2_content .gridcontainer', 10, 10);
+    });
+
+    // проверяем кнопку галерея
+    it('check button gallery', () => {
+        // проверяем, что кнопка есть в дом и отображается
+        browser.waitForExist(wayButton);
+        browser.waitForVisible(wayButton);
+        // проверяем, что текс кнопки Галерея
+        const getTest = browser.getText(wayButton);
+        expect(getTest).to.be.equal("Галерея");
+    });
+
+    // переходим в галерею
+    it('click button gallery', () => {
+        // кликаем по кнопке 
+        browser.click(wayButton);
+        // проверяем, что открылост окно с картинками
+        browser.isVisible('.slick-slide.slick-active img');
+    });
+
+    // проверяем, что в галереии отображаются все картинки
+    it('check image in gallery', () => {
+        // ждём загрузки страницы и появлениния кнопок галереии
+        browser.waitForVisible('.mk002__dots button');
+        // считаем количество картинок
+        const numberImage = $$('.mk002__dots button').length;
+        for(let i = 2; i <= numberImage; i++) {
+            // листаем картинки
+            browser.click(`.mk002__carousel li:nth-child(${i}) button`);
+            browser.pause(1000);
+            // проверяем что пропадает лоадер
+            browser.waitUntil(
+                () => browser.isVisible('.avn007-4_loader-wrapper.is_visible > div > div') === false,
+                20000, `${i} по счёту картинка не отображается`);
+        }
+        // проверяем количество классов 
+        const numberAtributs = $$('.slick-slide[aria-hidden="true"]').length;
+        // проверяем что количетсво выключенных лоадеров совпадает с количеством картинок
+        expect(numberAtributs).to.be.equal(numberImage);
+    });
+});
