0bc6c742b75944b522a80ce9bdeed9bda8869914
Kodix <Kodix>
2018-10-05 19:15:58 +0300

    Доработал тест по проверке карточек по городам

--- test/specs/businessLogic/filter/city-selection.js
+++ test/specs/businessLogic/filter/city-selection.js
@@ -1,4 +1,6 @@
 describe('test get all name city', () => {
+    // получаем количетсво городов в списке
+    const getListCity = () => $$('.avn005-2_citylist_item');
     const list = [];
     before('open page location', () => {
         browser.helpers.openSite();
@@ -23,7 +25,9 @@ describe('test get all name city', () => {
 
     // получаем список городов
     it('get list city', () => {
-        for(let i = 2; i <= 80; i++ ) {
+        const getNumberCity = getListCity().length;
+        console.log(getNumberCity);
+        for(let i = 2; i <= getNumberCity; i++ ) {
             const getCity = browser.getText(`.avn005-2_citieslist_inner div:nth-child(${i})`);
             list.push(getCity);
         }
@@ -32,21 +36,26 @@ describe('test get all name city', () => {
 
     // проверяем выборку каждого города, на наличие карточек с другим городом
     it('Check the city in the card', () => {
-        for(let i = 2; i <= 80; i++ ) {
+        const getNumberCity = getListCity().length;
+        for(let i = 2; i <= getNumberCity; i++ ) {
             // переносим курсор на нужный город
             browser.moveToObject(`.avn005-2_citieslist_inner div:nth-child(${i})`);
+            // получаем имя города
+            const nameCityList = browser.getText(`.avn005-2_citieslist_inner div:nth-child(${i})`);
             // выбираем город
             browser.click(`.avn005-2_citieslist_inner div:nth-child(${i})`);
             // ожидаем перерекдеринг карточек
             browser.pause(1000);
+            // проверяем на наличие предупреждения, что в городе машин
+            browser.waitUntil(
+                () => browser.isVisible('.avn001_not-found__title') === false,
+                5000, `При выборе города ${nameCityList} список машин не отразился`);
             // проверяем город первой карточки
             const nameCity = browser.getText('div:nth-child(1) > div > div > div > div:nth-child(4) > div > div > a > div');
-            console.log(nameCity);
             const nameCityString = nameCity.join();
+            // получаем город из всей надписи
             let getNameCityArray = list[i-2];
-            console.log(getNameCityArray);
             const getNameCity = nameCityString.match(getNameCityArray);
-            console.log(getNameCity);
             expect(getNameCity[0]).to.be.include(getNameCityArray);
 
             // открываем окно выбора метоположения
