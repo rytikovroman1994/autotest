3b61dfbfac68bde50105bf60fb3b04bc3bcea11f
Kodix <Kodix>
2018-10-08 13:35:40 +0300

    Починил тест для кнопки забронировать

--- test/specs/carsAvailable/basicField/button-book.js
+++ test/specs/carsAvailable/basicField/button-book.js
@@ -25,44 +25,60 @@ describe('test button to book', () => {
         // кликаем на кнопку
         browser.click(buttonClass);
         // ждём пока прогрузится картинка
-        browser.waitForVisible('.avn007-1_main-stage img')
+        browser.waitForVisible('.uac001_image-status img')
         // проверяем, что перешли на страницу заполнения формы
-        browser.waitForVisible('.op005_register form');
+        browser.waitForVisible('.op005_form_first');
+        // проверяем, что кнопка отправить заказ отображается
+        browser.waitForVisible('.op005_form_first .op005_form-btn');
+        // переходим на след страницу
+        browser.click('.op005_form_first .op005_form-btn');
+        // ждём пока загрузится авторизация в ЛК
+        browser.waitForVisible('.cblock');
+        // переводим курсор на кнопку зарегестрироваться 
+        browser.moveToObject('.cblock__overhang_short div:nth-child(2)');
+        // кликаем по кнопке зарегестрироваться
+        browser.click('.cblock__overhang_short div:nth-child(2)');
     });
 
-    // заполняем форму
-    it('fill out the booking form', () => {
+    it('verification of email input', () => {
+        browser.waitForExist('#email');
+        // вводидим эмеил
+        browser.setValue('#email', faker.internet.email(1));
+    });
+    it('checking phone input', () => {
+        // вводим телефонный номер
+        browser.waitForExist('#mobile');
+        browser.setValue('#mobile', faker.phone.phoneNumber(0));
+    });
+    it('checking the last name', () => {
         // выбираем гендер
-        browser.leftClick('.radiobtn__control[value="М"]');
-        // вводим имя
-        browser.setValue('.input__field[placeholder="Введите ваше имя"]', faker.name.firstName());
+        browser.click('.form__radio span');
+        browser.waitForExist('#lastName');
         // вводим фамилию
-        browser.setValue('.input__field[placeholder="Фамилия"]', faker.name.lastName());
-        // вводим отчество 
-        browser.setValue('.input__field[placeholder="Отчество"]', faker.name.lastName());
-        // вводим номер телефона
-        browser.setValue('.input__field[placeholder="Телефон"]', faker.phone.phoneNumberFormat(0));
-        if (browser.isVisible('.is_filled.is_invalid') === true) {
-            // очищаем поле
-            browser.clearElement('.input__field[placeholder="Телефон"]');
-            // вводим новый телефоннный номер
-            browser.setValue('.input__field[placeholder="Телефон"]', faker.phone.phoneNumberFormat(0));
-        }
-        // вводим эмеил
-        browser.setValue('.input__field[placeholder="Email"]', faker.internet.email());
-        
-        // проверяет, что пропала неактивная кнопка 
-        browser.waitUntil(
-            () => browser.isVisible('.btn_cta.is_disabled') === false,
-            5000, "Кнопка Отправить заказ всё ещё не активна");
-        // проверяем что существует кнопка "Отправить заказ" и что она отображается на странице
-        browser.waitForExist('.btn_with_loader ');
-        browser.waitForVisible('.btn_with_loader ');
+        browser.setValue('#lastName', faker.name.lastName(1));
     });
-
-    // проверяем кликбельность кнопки "Отправить заказ" и переход на нудную страницу
-    it('check order submit button', () => {
-        // кликаем на кнопку
-        browser.click('.btn_with_loader');
+    it('input check name', () => {
+        // вводим имя
+        browser.setValue('#firstName', faker.name.firstName(1));
+    });
+    it('entry check patronymic', () => {
+        // вводим отчество
+        browser.setValue('#patronomic', faker.name.lastName(1));
+    });
+    it('enter the password', () => {
+        const password = faker.internet.password(10);
+        // вводим пароль
+        browser.setValue('#password', password);
+        // подтверждаем пароль
+        browser.setValue('#password-confirm', password);
+    });
+    it('check that we are back', () => {
+        // кликаем по кнопке "Зарегестрироваться"
+        browser.click('.btn_cta[type="submit"]');
+        // ожидаем загрузку карточки
+        browser.waitForExist('.cblock');
+        // Получаем текст, что нужно подтвердить емаил
+        const getText = browser.getText('.cblock__title');
+        expect(getText).to.be.equal('Подтверждение адреса E-mail');
     });
 });
