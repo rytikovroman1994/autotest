d09e2ef52177e9119674d88178b3781ec07394bb
Kodix <Kodix>
2018-10-12 18:41:08 +0300

    Улучшил тесты с помощью хелперов

--- test/specs/carsAvailable/filter/options/winterPackage/parking-heater.js
+++ test/specs/carsAvailable/filter/options/winterPackage/parking-heater.js
@@ -1,9 +1,11 @@
 describe('test heated Parking heater', () => {
+    // выносим часто используемое название условия комплектации
+    let conditions = 'Стояночный отопитель';
     const ctx = {
         originalScreenshot: null,
         newScreenshot: null,
       };
-    before(' open page options', () => {
+    before('open page options', () => {
         browser.helpers.openSite();
         // открываем страницу опции
         browser.click('.avn008_filter__tab[data-name="Опции"]');
@@ -11,32 +13,15 @@ describe('test heated Parking heater', () => {
         browser.click('.avn008_filter__second-tab[data-name="Зимний пакет"]');
     });
 
-     it('check checkbox heated Parking heater', () => {
-        // проверяем что фильтр пуст
-        browser.waitUntil(
-            ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
-        5000, "На странице уже есть одно условие фильтра");
-        // двигаем курсор до кнопки
-        browser.moveToObject('.checkbox[data-name="Стояночный отопитель"]');
-        // включаем чекбокс
-        browser.click('.checkbox[data-name="Стояночный отопитель"]');
-        // проверяем, что в фильтре появилось условие
-        browser.waitForExist('.avn008_filter-value-item_image');
-        // проверяем, что это именно фаркоп
-        const text = browser.getText('.avn008_filter-value-item_text__bottom');
-        expect(text).to.be.equal('СТОЯНОЧНЫЙ ОТОПИТЕЛЬ');
-        // убираем условие
-        browser.click('.checkbox[data-name="Стояночный отопитель"]');
-        // проверяем, что условие пропало
-        browser.waitUntil(
-            ()=> browser.isVisible('.avn008_filter-value-item_image') === false,
-        5000, "На странице уже есть одно условие фильтра");
+     it(`Check checkbox heated ${conditions}`, () => {
+        // проверяем работу чекбокса
+        browser.helpers.checkCheckbox(conditions, 'СТОЯНОЧНЫЙ ОТОПИТЕЛЬ');
      });
 
-     it('check more in detail Parking heater', () => {
-        browser.moveToObject('.avn008_option-check_more[data-name="checkbox%Стояночный отопитель"]');
+     it(`Check more in detail ${conditions}`, () => {
+        browser.moveToObject(`.avn008_option-check_more[data-name="checkbox%${conditions}"]`);
         // открываем всплывающее окно
-        browser.click('.avn008_option-check_more[data-name="checkbox%Стояночный отопитель"]');
+        browser.click(`.avn008_option-check_more[data-name="checkbox%${conditions}"]`);
         // ждём появления картинки
         browser.waitForVisible('.avn015_content .image-container');
         // берём скриншот с локала
@@ -45,11 +30,18 @@ describe('test heated Parking heater', () => {
         ctx.newScreenshot = browser.screenshot().value;
      });
 
-     it('compare screenshots', async () => {
+     it('Compare screenshots', async () => {
         expect(ctx.originalScreenshot).not.equal(null);
         expect(ctx.newScreenshot).not.equal(null);
     
         const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
         expect(distance).to.be.below(0.1);
+      });
+
+      // проверяем, что условие появилось в деталке машины
+      it('Check the equipment in detail', () => {
+        const newArray = browser.helpers.checkConditions(conditions, conditions);
+        // проверяем
+        expect(newArray).to.be.equal(conditions);
       });
 });
