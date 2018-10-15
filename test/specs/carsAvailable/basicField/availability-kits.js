ef84fccfaa9d861dad6e853a986bf728d2878ee5
Kodix <Kodix>
2018-10-15 12:25:43 +0300

    Доработал тест проверки комплектаций

--- test/specs/carsAvailable/basicField/availability-kits.js
+++ test/specs/carsAvailable/basicField/availability-kits.js
@@ -22,11 +22,18 @@ describe(' test availability of kits', () => {
             // считаем сколько в карточке комплектаций
             const equipment = $$(`.slick-slider> div > div > div:nth-child(${i}) .icon-nextstep-checkmark`).length;
             console.log(equipment);
+            // получаем имя модели 
+            const nameModel = browser.getText(`.slick-initialized > div > div > div:nth-child(${i}) .avn008_car__title`);
+
             for(let y = 2; y <= equipment + 1; y++ ) {
                 // нажимаем на чекбокс комплектации
                 browser.click(`.slick-initialized > div > div > div:nth-child(${i}) label:nth-child(${y}) > div`);
+                // получаем имся комплектации
+                const nameEquipment = browser.getText(`div:nth-child(${i}) > div > div > div > div:nth-child(3) > div > label:nth-child(${y})  .avn008_car__kit-title`);
                 // проверяем, что появилось условие в фильтре
-                browser.waitForExist('.avn008_filter-value-item');
+                browser.waitUntil(
+                    () => browser.isVisible('.avn008_filter-value-item') === true,
+                    5000, `Модель ${nameModel} с комплектацией ${nameEquipment} не выбирается`);
                 // ждём пока загрузится картинка условия
                 browser.waitForVisible('.avn008_filter-value-item_image');
                 // убираем условие комплектации
