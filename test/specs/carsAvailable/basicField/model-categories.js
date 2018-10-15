d9375618d12a9e34cd48a839c1c96c2866f0531e
Kodix <Kodix>
2018-10-15 14:13:15 +0300

    Доработал тест проверяющий модели в категориях

--- test/specs/carsAvailable/basicField/model-categories.js
+++ test/specs/carsAvailable/basicField/model-categories.js
@@ -15,9 +15,15 @@ describe('test model categories cars', () => {
 
     // допустимые модели внедорожника
     const SUV = [
+        'Teramont',
         'Tiguan',
         'Touareg',
         'Новый Touareg'
+    ];
+
+    // допустимые мождели хэтчбэка
+    const hatchback = [
+        'Golf'
     ];
 
     Array.prototype.diff = function(a) {
@@ -30,14 +36,27 @@ describe('test model categories cars', () => {
 
     // проверяем что в категории седан, только седаны
     it('check categories sedan', () => {
+        // создаём массив
+        const listSedan =[];
         // перехоим на вкладку седан
         browser.click('.avn008_model_tabs div:nth-child(2)');
-        // проверяем, что все карточки имеют допустимое для седанов название
-        const getNameModel = browser.getText('.avn008_car__title');
-        console.log(getNameModel);
+        // получаем количетсво моделей в категории
+        const numberModel = browser.getText('.avn008_car__title');
+        console.log(numberModel);
+        for( let i = 1; i <= numberModel.length; i++ ) {
+            // проверяем, что все карточки имеют допустимое для седанов название
+            const getNameModel = browser.getText(`div:nth-child(${i}) > div > div > div > h4`);
+            console.log(getNameModel);
+            listSedan.push(getNameModel);
+            if(browser.isVisible('.slick-next') === true) {
+                if(browser.isVisible('.slick-next .slick-disabled') === false)
+                // двигаем слайдер
+                browser.click('.slick-next');
+            }
+        }
         
         // проверяем совпадение массивов
-        const result = sedan.diff(getNameModel);
+        const result = sedan.diff(listSedan);
         // считаем количество несовпадений между массивами
         const emptyArray = result.length;
         // количество элементов в массиве не должно быть больше 0
@@ -48,14 +67,27 @@ describe('test model categories cars', () => {
 
     // проверяем что в категории универсал, только универсалы
     it('check categories MPV', () => {
-        // перехоим на вкладку универсал
+        // создаём массив
+        const listSedan =[];
+        // перехоим на вкладку седан
         browser.click('.avn008_model_tabs div:nth-child(3)');
-        // проверяем, что все карточки имеют допустимое для седанов название
-        const getNameModel = browser.getText('.avn008_car__title');
-        console.log(getNameModel);
+        // получаем количетсво моделей в категории
+        const numberModel = browser.getText('.avn008_car__title');
+        console.log(numberModel);
+        for( let i = 1; i <= numberModel.length; i++ ) {
+            // проверяем, что все карточки имеют допустимое для седанов название
+            const getNameModel = browser.getText(`div:nth-child(${i}) > div > div > div > h4`);
+            console.log(getNameModel);
+            listSedan.push(getNameModel);
+            if(browser.isVisible('.slick-next') === true) {
+                if(browser.isVisible('.slick-next .slick-disabled') === false)
+                // двигаем слайдер
+                browser.click('.slick-next');
+            }
+        }
         
         // проверяем совпадение массивов
-        const result = MPV.diff(getNameModel);
+        const result = MPV.diff(listSedan);
         // считаем количество несовпадений между массивами
         const emptyArray = result.length;
         // количество элементов в массиве не должно быть больше 0
@@ -66,19 +98,66 @@ describe('test model categories cars', () => {
 
     // проверяем что в категории внедорожники, только внедорожники
     it('check categories SUV', () => {
-        // перехоим на вкладку универсал
+        // создаём массив
+        const listSedan =[];
+        // перехоим на вкладку седан
         browser.click('.avn008_model_tabs div:nth-child(4)');
-        // проверяем, что все карточки имеют допустимое для седанов название
-        const getNameModel = browser.getText('.avn008_car__title');
-        console.log(getNameModel);
+        // получаем количетсво моделей в категории
+        const numberModel = browser.getText('.avn008_car__title');
+        console.log(numberModel);
+        for( let i = 1; i <= numberModel.length; i++ ) {
+            // проверяем, что все карточки имеют допустимое для седанов название
+            const getNameModel = browser.getText(`div:nth-child(${i}) > div > div > div > h4`);
+            console.log(getNameModel);
+            listSedan.push(getNameModel);
+            if(browser.isVisible('.slick-next') === true) {
+                if(browser.isVisible('.slick-next .slick-disabled') === false)
+                // двигаем слайдер
+                browser.click('.slick-next');
+            }
+        }
         
         // проверяем совпадение массивов
-        const result = SUV.diff(getNameModel);
+        const result = SUV.diff(listSedan);
         // считаем количество несовпадений между массивами
         const emptyArray = result.length;
         // количество элементов в массиве не должно быть больше 0
         browser.waitUntil(
             () => (emptyArray == 0) === true,
             5000, `${result[0]} не является внедорожником`);
+    });
+
+    // проверяем что в категории внедорожники, только внедорожники
+    it('check categories hatchback', () => {
+        let numberModelArray;
+        // создаём массив
+        const listSedan =[];
+        // перехоим на вкладку седан
+        browser.click('.avn008_model_tabs div:nth-child(5)');
+        // получаем количетсво моделей в категории
+        const numberModel = browser.getText('.avn008_car__title');
+        if((typeof numberModel == 'string') === true) {
+            numberModelArray = 1;
+        }
+        for( let i = 1; i <= numberModelArray; i++ ) {
+            // проверяем, что все карточки имеют допустимое для седанов название
+            const getNameModel = browser.getText(`div:nth-child(${i}) > div > div > div > h4`);
+            console.log(getNameModel);
+            listSedan.push(getNameModel);
+            if(browser.isVisible('.slick-next') === true) {
+                if(browser.isVisible('.slick-next .slick-disabled') === false)
+                // двигаем слайдер
+                browser.click('.slick-next');
+            }
+        }
+        
+        // проверяем совпадение массивов
+        const result = hatchback.diff(listSedan);
+        // считаем количество несовпадений между массивами
+        const emptyArray = result.length;
+        // количество элементов в массиве не должно быть больше 0
+        browser.waitUntil(
+            () => (emptyArray == 0) === true,
+            5000, `${result[0]} не является хэтчбеком`);
     });
 });
