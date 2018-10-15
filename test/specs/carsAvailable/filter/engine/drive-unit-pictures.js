635436c2428f18ea81e904c54d5cbec7340ee4cd
Kodix <Kodix>
2018-10-15 16:18:58 +0300

    Тест на проверку наличия условия привода в карточке

---
+++ test/specs/carsAvailable/filter/engine/drive-unit-pictures.js
@@ -0,0 +1,57 @@
+describe('test transmission in pictures', () => {
+    before('open page filter', () => {
+        browser.helpers.openSite();
+    });
+
+    // проверяем наличие условия и появления его в фильтре
+    it('Check checkbox four-wheel drive', () => {
+        // переходим на страницу двигатель 
+        browser.click('.avn008_filter__tab[data-name="Двигатель"]');
+        // нажимает на чекбокс 
+        browser.click('.checkbox[data-name="Передний привод"]');
+        // проверяем что появился пункт в фильтре
+        browser.waitForExist('.avn008_filter-value-item__with-image');
+        // проверяем что это именно автоматическая
+        expect(browser.getText('.avn008_filter-value-item_text__bottom')).to.be.equal('ПЕРЕДНИЙ');
+
+        // проверяем, что кнопка "Показать" активна
+        browser.waitForVisible('.avn008_overlay_bar_content .avn008_overlay_submit-block_btn');
+        // переходим к списку 
+        browser.click('.avn008_overlay_bar_content .avn008_overlay_submit-block_btn');
+        // проверяем, что в карточке есть условие АКП
+        const getView = browser.getText('.avn001_display__enable-hover > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div:nth-child(3) > div > div > div:nth-child(1) > div > div > div:nth-child(3) > div.avn001-2_specs-item_text');
+        expect(getView).to.be.include('FWD');
+    });
+
+    // переъодим обратно в фильтр
+    it('open filter', () => {
+        browser.click('#prompt-toggler_filter');
+        // переходим на страницу двигатель 
+        browser.click('.avn008_filter__tab[data-name="Двигатель"]');
+        // отключаем условие автоматической трансмисии
+        browser.click('.checkbox[data-name="Передний привод"]');
+        // переходим к списку 
+        browser.click('.avn008_overlay_bar_content .avn008_overlay_submit-block_btn');
+        browser.click('#prompt-toggler_filter');
+    });
+
+    // проверяем наличие условия и появления его в фильтре
+    it('Check checkbox front-wheel drive', () => {
+        // переходим на страницу двигатель 
+        browser.click('.avn008_filter__tab[data-name="Двигатель"]');
+        // нажимает на чекбокс 
+        browser.click('.checkbox[data-name="Полный привод"]');
+        // проверяем что появился пункт в фильтре
+        browser.waitForExist('.avn008_filter-value-item__with-image');
+        // проверяем что это именно автоматическая
+        expect(browser.getText('.avn008_filter-value-item_text__bottom')).to.be.equal('ПОЛНЫЙ');
+
+        // проверяем, что кнопка "Показать" активна
+        browser.waitForVisible('.avn008_overlay_bar_content .avn008_overlay_submit-block_btn');
+        // переходим к списку 
+        browser.click('.avn008_overlay_bar_content .avn008_overlay_submit-block_btn');
+        // проверяем, что в карточке есть условие АКП
+        const getView = browser.getText('.avn001_display__enable-hover > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div:nth-child(3) > div > div > div:nth-child(1) > div > div > div:nth-child(3) > div.avn001-2_specs-item_text');
+        expect(getView).to.be.include('4WD');
+    });
+});
