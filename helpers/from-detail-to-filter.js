5c1e087ccfa6ade9758a9f43b431a0a275cc9f0a
Kodix <Kodix>
2018-10-18 16:09:34 +0300

    Написал новый хелпер

---
+++ helpers/from-detail-to-filter.js
@@ -0,0 +1,30 @@
+/**
+ * @memberOf Helpers
+ * @function fromDetailToFilter
+ * @param {string} conditions - дата атрибут конткретного элемента комплектации
+ * @param {string} conditionsFilter - title условия в фильтре
+ * @example
+ *      browser.helpers.checkCheckbox('Интерьер', 'Руль', 'Электропривод сидений);
+ */
+
+export default function fromDetailToFilter(filterOne, conditions, filterToo) {
+    // проверяем что кнопка существует 
+    browser.waitForVisible('.avn012_link-back');
+    // наводим курсор на данную кнопку
+    browser.moveToObject('.avn012_link-back');
+    // выходим из деталки 
+    browser.click('.avn012_link-back');
+    // скролим вверх, потому что при переходе съезжает
+    browser.scroll(0, -300);
+    // ожидаем появлеия кнопки
+    browser.waitForVisible('#prompt-toggler_filter');
+    // возвращаетмся в фильтр
+    browser.click('#prompt-toggler_filter');
+    // переходим на страницу интерьера
+    browser.click(`.avn008_filter__tab[data-name="${filterOne}"]`);
+    if(filterToo != undefined) {
+        browser.click(`.avn008_filter__second-tab[data-name="${filterToo}"]`);
+    }
+    // убираем условие
+    browser.click(`.checkbox[data-name="${conditions}"]`);
+}
