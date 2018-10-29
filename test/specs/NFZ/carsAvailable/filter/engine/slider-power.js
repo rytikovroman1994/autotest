3f928f61061dc10e70c14c389945a85cd2358ef2
Kodix <Kodix>
2018-10-29 13:31:50 +0300

    Тест на проверку работы слайдера в разделе можность двигателя

---
+++ test/specs/NFZ/carsAvailable/filter/engine/slider-power.js
@@ -0,0 +1,42 @@
+describe('test slider power', () => {
+
+    // допустимая мощность двигателей
+    const list = [
+        '204 - 224',
+        '150 - 180',
+        '102 - 140',
+        '102 - 224'
+    ];
+
+    before('open page filter', () => {
+        browser.helpers.openFilter();
+    });
+
+    // переходим на страницу "Двигатель"
+    it('Check open page engine', () => {
+        // открываем страницу двигатель
+        browser.click('#react-tabs-2');
+        // ожтдаем закрузки послденей картинки
+        browser.waitForVisible('.grid_m_2 img');
+        // провеярем, что появился слайдер
+        browser.waitUntil(
+            () => browser.isVisible('.avn008_engine__card--power') === true,
+            5000, "Поле с слайдером мощности не отображается");
+    });
+
+    // проверяем работу слайдера
+    it('Check slider power', () => {
+        for( let i = 1; i <= 4; i++ ) {
+            // выбираем мощность двигателя
+            browser.click(`.rc-slider-step > span:nth-child(${i})`);
+            // проверяем, что в фильтре появилось условие
+            browser.waitUntil(
+                () => browser.isExisting('.avn008_filter-value-item') === true,
+                5000, "Условие с мощностью двигателя не отобразтлоась в фильтре");
+            // получаем мощность двигателя в фильтре
+            const powerFilter = browser.getText('.avn008_filter-value-item_text-self');
+            // проверяем что в фильтре отображается правильная мощность
+            expect(powerFilter).to.be.equal(list[i-1]);
+        }
+    });
+});
