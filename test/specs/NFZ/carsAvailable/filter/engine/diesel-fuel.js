810dedd4935804949722d8133eecd37533b0c4f8
Kodix <Kodix>
2018-10-29 18:12:15 +0300

    Тесты на чекбоксов в категории топливо

--- test/specs/NFZ/carsAvailable/filter/engine/diesel-fuel.js
+++ test/specs/NFZ/carsAvailable/filter/engine/diesel-fuel.js
@@ -1,13 +1,13 @@
-describe('test rear-wheel-drive', () => {
+describe('test diesel-fuel', () => {
     before('open page filter', () => {
         browser.helpers.openFilter();
         // открываем страницу "Двигатель"
         browser.click('#react-tabs-2');
     });
 
-    // выбираем задний привод
-    it('Check rear-wheel-drive', () => {
-        browser.helpers.checkCheckboxNfz('div:nth-child(3) > div > label:nth-child(5)', 'ЗАДНИЙ');
+    // выбираем топливо дизель
+    it('Check diesel-fuel', () => {
+        browser.helpers.checkCheckboxNfz('div:nth-child(4) > div > label:nth-child(3)', 'ДИЗЕЛЬ');
     });
 
     // ожидаем, пока загрузится первая карточка и проверяем наличие комплектации
@@ -17,7 +17,7 @@ describe('test rear-wheel-drive', () => {
         // ожидаем, пока перерендерится список карточек
         browser.pause(2000);
         // проверяем, что в карточке есть условие АКП
-        const getView = browser.getText('.avn001_display__enable-hover > div:nth-child(1) > div > div > div:nth-child(1) .gridcontainer > div:nth-child(4) > div > div > div:nth-child(1) > div > div > div:nth-child(3) > div.avn001-2_specs-item_text');
-        expect(getView).to.be.include('RWD');
+        const getView = browser.getText('.avn001_display__enable-hover > div:nth-child(1) > div > div > div:nth-child(1) .gridcontainer > div:nth-child(4) > div > div > div:nth-child(1) > div > div > div:nth-child(1) > div.avn001-2_specs-item_text');
+        expect(getView).to.be.include('TDI');
     });
 });
