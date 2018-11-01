e48caa8402392f0254757203bbfcedc5011c371b
Kodix <Kodix>
2018-11-01 18:57:20 +0300

    Тест  добавления нескольких цветов

---
+++ test/specs/NFZ/carsAvailable/color/body-color-all.js
@@ -0,0 +1,41 @@
+describe('test body color', () => {
+    const color = () => $$('.avn008_filter-value-item');
+    // начальное число цветов
+    let initialNumberColor;
+    // текущее число цветов
+    let currentNumberColor;
+    before('open page filter', () => {
+        browser.helpers.openFilter();
+    });
+
+    // переходим нас траницу Цвет
+    it('Open page Color', () => {
+        // ожидаем пока появится кнопка Цвет
+        browser.waitForVisible('body #react-tabs-4');
+        // переходим в раздел Цвет
+        browser.click('body #react-tabs-4');
+        // ожидаем появления картинки дверей
+        browser.waitForVisible('.avn008_image-switcher_image');
+    });
+
+    it('Check the color of the door', () => {
+        // скролим страницу
+        browser.scroll('.avn008_image-switcher_image', 0, 200);
+        // запоминаем начальное кол-во 
+        initialNumberColor = color().length;
+        for( let i = 1;  i <= 8; i++ ) {
+            if( browser.isVisible(`.avn008_color-colors > div:nth-child(${i})`) === true) {
+            // выбираем цвет 
+            browser.click(`.avn008_color-colors > div:nth-child(${i})`);
+            // получаем текущие кол-во цветов в фильтре
+            currentNumberColor = color().length;
+            // проверяем, что стало на 1 больше
+            browser.waitUntil(
+                () => currentNumberColor == (initialNumberColor + 1) === true,
+                5000, `${i} цвет не добавился в фильтр`);
+
+            initialNumberColor = currentNumberColor;
+            }
+        }
+    });
+});
