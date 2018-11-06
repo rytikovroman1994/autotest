edf6836e141ebe0ca54db7a54a4a20a71b7b4bce
Kodix <Kodix>
2018-11-06 15:44:09 +0300

    Тест на проверку чекбоксов в категории Подушки безопасности

---
+++ test/specs/NFZ/carsAvailable/filter/interior/airbag/one.js
@@ -0,0 +1,22 @@
+describe('test airbag one', () => {
+    // выносим часто используемое название условия комплектации
+    let conditions = '1';
+    before('open page filter', () => {
+        browser.helpers.openFilter();
+        // проверяем переход на страницу Интерьер
+        browser.click('body #react-tabs-8');
+    }); 
+
+    // проверяем работу чекбоскса
+    it(`Check checkbox ${conditions}`, () => {
+        // проверяем работу чекбокса
+       browser.helpers.checkCheckboxNfz(conditions, '1');
+    });
+
+    // проверяем, что условие появилось в деталке машины
+    it('Check the equipment in detail', () => {
+        const newArray = browser.helpers.checkConditionsNfz(conditions, conditions);
+        // проверяем
+        expect(newArray).to.be.equal(conditions);
+    });
+});
