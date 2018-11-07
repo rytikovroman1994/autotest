46d8292435d9d5f8900ffa20f46ffd83d0b14bd4
Kodix <Kodix>
2018-11-07 18:17:33 +0300

    Тесты чекбоксов в разделе Грузоподъёмность

---
+++ test/specs/NFZ/carsAvailable/filter/exterior/carrying/over-one-ton.js
@@ -0,0 +1,22 @@
+describe('test carrying to one ton', () => {
+    // выносим часто используемое название условия комплектации
+    let conditions = 'Свыше 1т';
+    before('open page filter', () => {
+        browser.helpers.openFilter();
+        // проверяем переход на страницу Интерьер
+        browser.click('.avn008_filter__tab[data-name="Экстерьер"]');
+    }); 
+
+    // проверяем работу чекбоскса
+    it(`Check checkbox ${conditions}`, () => {
+        // проверяем работу чекбокса
+       browser.helpers.checkCheckboxNfz(conditions, 'СВЫШЕ 1Т');
+    });
+
+    // проверяем, что условие появилось в деталке машины
+    it('Check the equipment in detail', () => {
+        const newArray = browser.helpers.checkConditionsNfz(conditions, conditions);
+        // проверяем
+        expect(newArray).to.be.equal(conditions);
+    });
+});
