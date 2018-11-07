d8f5479f164fbbbb3a3993ab4411c8771f2c08c9
Kodix <Kodix>
2018-11-07 17:44:26 +0300

    Тесты на проверку чекбоксов в Категории Подвеска

---
+++ test/specs/NFZ/carsAvailable/filter/exterior/suspension/adaptive.js
@@ -0,0 +1,22 @@
+describe('test suspension adaptive', () => {
+    // выносим часто используемое название условия комплектации
+    let conditions = 'Адаптивная';
+    before('open page filter', () => {
+        browser.helpers.openFilter();
+        // проверяем переход на страницу Интерьер
+        browser.click('.avn008_filter__tab[data-name="Экстерьер"]');
+    }); 
+
+    // проверяем работу чекбоскса
+    it(`Check checkbox ${conditions}`, () => {
+        // проверяем работу чекбокса
+       browser.helpers.checkCheckboxNfz(conditions, 'АДАПТИВНАЯ');
+    });  
+
+    // проверяем, что условие появилось в деталке машины
+    it('Check the equipment in detail', () => {
+        const newArray = browser.helpers.checkConditionsNfz(conditions, conditions);
+        // проверяем
+        expect(newArray).to.be.equal(conditions);
+    });
+});
