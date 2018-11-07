941084b646b4c5df464902d42a5c611b8539b099
Kodix <Kodix>
2018-11-07 18:50:28 +0300

    Тест чекбоксов в разделе Дополнительно

---
+++ test/specs/NFZ/carsAvailable/filter/exterior/additionally/conversion.js
@@ -0,0 +1,22 @@
+describe('test additionally conversion', () => {
+    // выносим часто используемое название условия комплектации
+    let conditions = 'Переоборудование';
+    before('open page filter', () => {
+        browser.helpers.openFilter();
+        // проверяем переход на страницу Интерьер
+        browser.click('.avn008_filter__tab[data-name="Экстерьер"]');
+    }); 
+
+    // проверяем работу чекбоскса
+    it(`Check checkbox ${conditions}`, () => {
+        // проверяем работу чекбокса
+       browser.helpers.checkCheckboxNfz(conditions, 'ПЕРЕОБОРУДОВАНИЕ');
+    });
+
+    // проверяем, что условие появилось в деталке машины
+    it('Check the equipment in detail', () => {
+        const newArray = browser.helpers.checkConditionsNfz(conditions, conditions);
+        // проверяем
+        expect(newArray).to.be.equal(conditions);
+    });
+});
