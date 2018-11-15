e42d91e64b9c8a7e736dd18e7cc9d9238893cf78
Kodix <Kodix>
2018-11-15 18:32:18 +0300

    Исправил комент

--- helpers/NFZ/open-filter-nfz.js
+++ helpers/NFZ/open-filter-nfz.js
@@ -1,5 +1,5 @@
 export default function openFilter() {
-    // делаем размер окна браузера максимальным
+    // делаем размер окна браузера
     browser.windowHandleSize ({width: 1366, height: 768});
     // переходим на страницу
     browser.url('https://nfz.kodix.ru');
