884c2fcab25ec2da1d5791c2d09fb6cd6a23c06d
Kodix <Kodix>
2018-11-15 14:05:55 +0300

    Доработал тест на проверку вида топлива в карточках

--- test/specs/PKW/carsAvailable/filter/engine/fuel-type-pictures.js
+++ test/specs/PKW/carsAvailable/filter/engine/fuel-type-pictures.js
@@ -1,4 +1,14 @@
 describe('test fuel type in pictures', () => {
+    const listPetrol = [
+        'TSI',
+        'MPI',
+        'FSI'
+    ]
+
+    Array.prototype.diff = function(a) {
+        return this.filter(function(i){
+            return a.indexOf(i) < 0;});
+    };
     before('open page filter', () => {
         browser.helpers.openSite();
     });
@@ -20,7 +30,7 @@ describe('test fuel type in pictures', () => {
             10000, "Кнопка Показать не активна в течении 10 секунд");
         browser.waitForVisible('.avn008_overlay_submit-block_btn .btn.btn_cta');
         // переходим к списку 
-        browser.click('.avn008_overlay_submit-block_btn .btn.btn_cta');
+        browser.click('.avn008_overlay_submit-block .btn__text');
         browser.pause(2000);
         // проверяем, что в карточке есть условие TDI
         const getView = browser.getText('.avn001_display__enable-hover > div:nth-child(1) > div > div > div:nth-child(1) .avn001-2_specs-item[data-type="catalogue_item_specs_engine"]');
@@ -38,7 +48,7 @@ describe('test fuel type in pictures', () => {
             () => browser.isVisible('avn008_overlay_bar--progress') === false,
             10000, "Кнопка Показать не активна в течении 10 секунд");
         // переходим к списку 
-        browser.click('.avn008_overlay_submit-block_btn .btn.btn_cta');
+        browser.click('.avn008_overlay_submit-block .btn__text');
         browser.click('body #prompt-toggler_filter');
     });
 
@@ -59,10 +69,18 @@ describe('test fuel type in pictures', () => {
         browser.waitUntil(
             () => browser.isVisible('avn008_overlay_bar--progress') === false,
             10000, "Кнопка Показать не активна в течении 10 секунд");
+        browser.pause(3000);
         // переходим к списку 
-        browser.click('.avn008_overlay_submit-block_btn .btn.btn_cta');
+        browser.click('.avn008_overlay_submit-block .btn__text');
         // проверяем, что в карточке есть условие MPI
         const getView = browser.getText('.avn001_display__enable-hover > div:nth-child(1) > div > div > div:nth-child(1) .avn001-2_specs-item[data-type="catalogue_item_specs_engine"]');
-        expect(getView).to.be.include('MPI');
+        // проверяем совпадение массивов
+        const result = [getView.split(' ')[0]].diff(listPetrol);
+        // считаем количество несовпадений между массивами
+        const emptyArray = result.length;
+        // количество элементов в массиве не должно быть больше 0
+        browser.waitUntil(
+            () => (emptyArray == 0) === true,
+            5000, `${result[0]} не входит в список доступных видов бензина`);
     });
 });
