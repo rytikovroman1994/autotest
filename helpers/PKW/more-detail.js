22cc9625810163f3c3a726bef2dd40fc7674ec57
Kodix <Kodix>
2018-11-20 18:01:17 +0300

    Доработал хелпер

--- helpers/PKW/more-detail.js
+++ helpers/PKW/more-detail.js
@@ -2,7 +2,6 @@
  * @memberOf Helpers
  * @function moreDetail
  * @param {string} conditions - дата атрибут конткретного элемента комплектации
- * @param {string} way - пусть к сриншоту на локале
  * @example
  *      browser.helpers.checkCheckbox('Active Info Display', '/screenshotOption/cruiseControl.png');
  */
@@ -12,5 +11,10 @@ export default function moreDetail(conditions) {
     // открываем всплывающее окно
     browser.click(`.avn008_option-check_more[data-name="${conditions}"]`);
     // ждём появления картинки
+    browser.waitUntil(
+        () => browser.isExisting('.image-lazyload_overlay.is_visible .content-loader__self') === false,
+        20000, `Картинка ${conditions} не отображается в Подробнее`);
     browser.waitForVisible('.avn015_content .image-container img');
+    // страховочная пауза
+    browser.pause(1000);
 }
