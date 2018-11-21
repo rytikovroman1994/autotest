b0559279450f4cec3f53cb4fdaad364d6b45c76e
Kodix <Kodix>
2018-11-21 20:10:44 +0300

    Тесты на проверку слайдеров на вкладке Лизинг

--- test/specs/NFZ/carsAvailable/filter/finance/Leasing/residual-payment.js
+++ test/specs/NFZ/carsAvailable/filter/finance/Leasing/residual-payment.js
@@ -1,13 +1,13 @@
-describe('test leasing initial payment', () => {
+describe('test leasing residual payment', () => {
     // выносим часто используемый класс
-    let selector = '.avn008_credit__firstPaySlider[data-name="Первоначальный платёж"]';
+    let selector = '.avn008_credit__firstPaySlider[data-name="Остаточный платёж"]';
     before('open page filter', () => {
         browser.helpers.openFilter();
         // переходим на страницу финансы
         browser.click('.avn008_filter__tab[data-name="Финансы"]');
     });
 
-    // проверяем переход на вкладку Лизинг и работу слайдера Первоначальный платёж
+    // проверяем переход на вкладку Лизинг и работу слайдера остаточный платёж
     it('Check slider initial paymen', () => {
         // переходим на вкладку Кредит
         browser.click('.rc-slider-step > span:nth-child(1)');
@@ -16,13 +16,13 @@ describe('test leasing initial payment', () => {
         // очищаем поле ввода суммы Первоначального платежа
         browser.clearElement(`${selector} .ci001-1_input`);
         // вводим свою сумму 
-        browser.addValue(`${selector} .ci001-1_input`, '3000000');
+        browser.addValue(`${selector} .ci001-1_input`, '1500000');
         // убираем фокус, чтобы сумма применилась
         browser.click('form');
         // проверяем, что слайдер поменял позицию 
         const finishPosition = browser.getLocation(`${selector} .range-slider-handle.range-slider-handle-2`);
         browser.waitUntil(
             () => (finishPosition != startPosition) === true,
-            5000, "Слайдер Первоначальный платёж не переместился при изменнеии суммы");
+            5000, "Слайдер Остаточный платёж не переместился при изменнеии суммы");
     });
 });
