26d10e1a210b18e9979eb5126564347ef4e2c557
Kodix <Kodix>
2018-12-03 17:04:47 +0300

    Хелпер на проверку по пикселям и сохрание картинки расхождения скринов

---
+++ helpers/PKW/screenshot-compare-diff.js
@@ -0,0 +1,21 @@
+import Jimp from 'jimp';
+
+/**
+ * @memberOf Helpers
+ * @function compareScreenshots
+ * @param {string} screenshotOne - First Base64 encoded screenshot in PNG format.
+ * @param {string} screenshotTwo - Second Base64 encoded screenshot in PNG format.
+ * @example
+ *  const distance = await browser.helpers.compareScreenshotsDiff(one, two);
+ */
+export default async function compareScreenshotsDiff(screenshotOne, screenshotTwo) {
+    // console.log(screenshotOne);
+    // данный способ нужен для чтения картинки с локала
+    const imageOne = await Jimp.read(screenshotOne);
+    // данный способ нужен для чтения картинки с буфера
+    const imageTwo = await Jimp.read(Buffer.from(screenshotTwo, 'base64'));
+    // сравниваем по пиксилям картинки
+    const diff = await Jimp.diff(imageOne, imageTwo);
+    // выводим разницу в процентном соотношении
+    return diff;
+}
