34c6ce07427f772aa9f97bdf16e5e71d00cade59
Kodix <Kodix>
2018-12-03 17:02:57 +0300

    Изменил хелпер

--- helpers/NFZ/open-list-nfz.js
+++ helpers/NFZ/open-list-nfz.js
@@ -4,7 +4,7 @@ export default function openListNfz() {
     // переходим на страницу
     browser.url('https://nfz.kodix.ru');
     // открываем фильтр
-    while(browser.isVisible('.avn008_display-item_content') === true) {
+    while(browser.isVisible('.react-tabs[data-tabs="true"]') === true) {
         browser.click('body #prompt-toggler_filter');
     }
 }
