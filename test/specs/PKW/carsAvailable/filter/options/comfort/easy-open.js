86db7c62200ce9ca16b74d03e955d7ad5fe0fea0
Kodix <Kodix>
2018-12-03 19:20:46 +0300

    Дополнил тесты проверкой пиксеелй

--- test/specs/PKW/carsAvailable/filter/options/comfort/easy-open.js
+++ test/specs/PKW/carsAvailable/filter/options/comfort/easy-open.js
@@ -32,7 +32,17 @@ describe('test easy-open', () => {
         expect(ctx.newScreenshot).not.equal(null);
     
         const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
-        expect(distance).to.be.below(0.1);
+        const diff = await browser.helpers.compareScreenshotsDiff(ctx.originalScreenshot, ctx.newScreenshot, '0');
+    
+        // expect(distance).to.be.above(0);
+        if(diff.percent > 0.01 || distance > 0.1) {
+            // если большое различие, то сохраняем изображение с отличием
+            diff.image.write(`./test/reports/allure-results/${conditions}.png`);
+            // проверяем допустипость отличия в пикселях
+            expect(diff.percent).to.be.below(0.01);
+            // проверем допустимость отличия в растоянии
+            expect(distance).to.be.below(0.1);
+        }
       });
 
       // проверяем, что условие появилось в деталке машины
