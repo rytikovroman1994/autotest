6b0f58d397d2d6de4c6d67349b24e1f3c832951c
Kodix <Kodix>
2018-12-05 18:26:45 +0300

    Лони алюра и правки

--- test/test-data/test-diff-images.js
+++ test/test-data/test-diff-images.js
@@ -1,20 +1,29 @@
-describe.skip('test diff imeges', () => {
+describe('test diff imeges', () => {
     const ctx = {
         originalScreenshot: null,
         newScreenshot: null,
       };
 
     before('open page filter', () => {
-        browser.helpers.openListNfz();
-        // переходим в деталку
-        // переходим в деталку 
-        browser.click('.avn001_display__enable-hover > div:nth-child(1) > div > div > div:nth-child(1) img');
-        // ждём появления картинки
-        browser.waitForVisible('.preview_img img', 60000);
+        // делаем размер окна браузера
+        browser.windowHandleSize ({width: 1100, height: 1200});
+        // переходим на страницу
+        browser.url('https://nfz.kodix.ru');
+        // открываем фильтр
+        while(browser.isVisible('.react-tabs[data-tabs="true"]') === false) {
+            browser.click('body #prompt-toggler_filter');
+        }
+        // переходим на страницу Интерьер
+        browser.click('.avn008_filter__tab[data-name="Цвет"]');
+        browser.execute(
+            () => document.getElementsByClassName('avn008_filter')[0].style.background = 'rgba(29,31,32,1)'
+        );
+        // ждём загрузку последне картинкии
+        browser.waitForVisible('.avn008_image-switcher_image');
     });
-
     // проверяем работу diff
     it('Test images', () => {
-        browser.saveElementScreenshot('./snapshot/diff/test3.png', '.detailCarInfo');
+        browser.saveScreenshot('./snapshot/diff/test4.png');
+        browser.saveElementScreenshot('./snapshot/diff/test3.png', '.avn008_display-item_content');
     }); 
 });
