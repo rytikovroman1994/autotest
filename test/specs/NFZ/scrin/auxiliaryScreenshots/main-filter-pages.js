0c3f3dc98be8c53d5c38fb3854b770a8841c0bd0
Kodix <Kodix>
2018-12-06 16:16:54 +0300

    Тест на сбор актуальных скриншотов

--- test/specs/NFZ/scrin/auxiliaryScreenshots/main-filter-pages.js
+++ test/specs/NFZ/scrin/auxiliaryScreenshots/main-filter-pages.js
@@ -1,4 +1,23 @@
-describe.skip('test nfz main filter pages', () => {
+describe('test nfz main filter pages', () => {
+    function diagonal(namePage) {
+        // делаем мобильный размер экрана
+        browser.windowHandleSize ({width: 400, height: 1200});
+        browser.pause(3000);
+        var screen = browser.saveScreenshot(`./snapshot/nfzMainFilterPages/400-${namePage}.png`);
+        expect(screen).to.not.equal(null);
+
+        // делаем планшетный размер экрана
+        browser.windowHandleSize ({width: 800, height: 1200});
+        browser.pause(3000);
+        var screen = browser.saveScreenshot(`./snapshot/nfzMainFilterPages/800-${namePage}.png`);
+        expect(screen).to.not.equal(null);
+
+        // делаем пк размер экрана
+        browser.windowHandleSize ({width: 1366, height: 1200});
+        browser.pause(3000);
+        var screen = browser.saveScreenshot(`./snapshot/nfzMainFilterPages/1366-${namePage}.png`);
+        expect(screen).to.not.equal(null);
+    }
     before('open first page', () => {
         browser.helpers.openFilter();
         // ожидаем пока загрузится последняя картинка модели
@@ -10,8 +29,7 @@ describe.skip('test nfz main filter pages', () => {
 
     // делаем скриншот страницы Модель
     it('Screenshot of the model page', () => {
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/model.png');
-        expect(screen).to.not.equal(null);
+        diagonal('model');
     });
 
     // делаем скриншот страницы Двигатель 
@@ -20,8 +38,8 @@ describe.skip('test nfz main filter pages', () => {
         browser.click('.avn008_filter__tab[data-name="Двигатель"]');
         // ждём пока загрузится последняя страница 
         browser.waitForVisible('.avn008_engine__card-image[data-name="Трансмиссия"]');
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/engine.png');
-        expect(screen).to.not.equal(null);
+
+        diagonal('engine');
     });
 
     // делаем скриншот страницы Цвет-Экстерьер 
@@ -30,18 +48,14 @@ describe.skip('test nfz main filter pages', () => {
         browser.click('.avn008_filter__tab[data-name="Цвет"]');
         // ждём пока загрузится последняя страница 
         browser.waitForVisible('.avn008_image-switcher_image');
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/color-door.png');
-        expect(screen).to.not.equal(null);
+        diagonal('color-door');
     });
 
     // делаем скриншот страницы Цвет-ИНтерьер
     it('Screenshot of the color page interier', () => {
         // переключаемся на интерьер
         browser.click('.grid_s_12:nth-child(1) .rc-slider-dot:nth-child(1)');
-        // ждём пока перерендерится картинка
-        browser.pause(3000);
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/color-seet.png');
-        expect(screen).to.not.equal(null);
+        diagonal('color-seet');
     });
 
     // делаем скриншот страницы Экстерьер 
@@ -50,8 +64,7 @@ describe.skip('test nfz main filter pages', () => {
         browser.click('.avn008_filter__tab[data-name="Экстерьер"]');
         // ждём пока загрузится последняя картинка 
         browser.waitForVisible('div:nth-child(4) img');
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/exterier.png');
-        expect(screen).to.not.equal(null);
+        diagonal('exterier');
     });
 
     // делаем скриншот страницы Интерьер
@@ -60,23 +73,34 @@ describe.skip('test nfz main filter pages', () => {
         browser.click('.avn008_filter__tab[data-name="Интерьер"]');
         // ждём пока загрузится последняя картинка 
         browser.waitForVisible('div:nth-child(4) img');
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/interier.png');
-        expect(screen).to.not.equal(null);
+        diagonal('interier');
     });
 
      // делаем скриншот страницы Финансы
      it('Screenshot of the page finance', () => {
         // переходим на страницу финансы 
         browser.click('.avn008_filter__tab[data-name="Финансы"]');
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/finance.png');
-        expect(screen).to.not.equal(null);
+        diagonal('finance');
+    });
+    
+    // делаем скриншот старницы Кредит
+    it('Screenshot of the page credit', () => {
+        // переходим на страницу кредит 
+        browser.click('.rc-slider-step > span:nth-child(2)');
+        diagonal('credit');
+    });
+
+    // делаем скриншот старницы Лизинг
+    it('Screenshot of the page leasing', () => {
+        // переходим на страницу кредит 
+        browser.click('.rc-slider-step > span:nth-child(1)');
+        diagonal('leasing');
     });
 
     // делаем скриншот страницы Дилеры
     it('Screenshot of the page dealers', () => {
         // переходим на страницу Дилеры 
         browser.click('.avn008_filter__tab[data-name="Дилеры"]');
-        var screen = browser.saveScreenshot('./snapshot/nfzMainFilterPages/dealers.png');
-        expect(screen).to.not.equal(null);
+        diagonal('dealers');
     });
 });
