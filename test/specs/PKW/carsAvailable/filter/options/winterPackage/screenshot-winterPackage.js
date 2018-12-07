cb87b9e9bbedb367f88459ca97bb52e3e119ccdf
Kodix <Kodix>
2018-12-07 13:35:24 +0300

    Добавил в тесты проверку по пикселям в трёх разрешениях

--- test/specs/PKW/carsAvailable/filter/options/winterPackage/screenshot-winterPackage.js
+++ test/specs/PKW/carsAvailable/filter/options/winterPackage/screenshot-winterPackage.js
@@ -4,6 +4,8 @@ describe('screenshot page winter package', () => {
         originalScreenshot: null,
         newScreenshot: null,
       };
+    // имя тестируемой страницы
+    let namePage = 'winterPackage';
     before(() => {
         browser.helpers.openSite();
         // переходим на страницу опции
@@ -14,31 +16,87 @@ describe('screenshot page winter package', () => {
         browser.waitForVisible('.avn008_option__slider-card-icon');
     });
 
-    it('take screenshot page winter package', () => {
+    it('Сompare screenshots 400', async () => {
+        // делаем мобильный размер экрана
+        browser.windowHandleSize ({width: 400, height: 1200});
+        browser.pause(3000);
         // берём скриншот с локала
-        ctx.originalScreenshot = 'snapshot/screenshotOption/winterPackage.png';
-    });
-
-    it('do second screenshot', () => {
+        ctx.originalScreenshot = `./snapshot/screenshotOption/400-${namePage}.png`;
         // делаем текущий скриншот
         ctx.newScreenshot = browser.screenshot().value;
-    });
 
-    it('compare screenshots', async () => {
         expect(ctx.originalScreenshot).not.equal(null);
         expect(ctx.newScreenshot).not.equal(null);
     
         const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
         const diff = await browser.helpers.compareScreenshotsDiff(ctx.originalScreenshot, ctx.newScreenshot, '0');
+        console.log(distance);
+        console.log(diff.percent);
     
         // expect(distance).to.be.above(0);
-        if(diff.percent > 0.01 || distance > 0.1) {
+        if(diff.percent > 0.04 || distance > 0.07) {
             // если большое различие, то сохраняем изображение с отличием
-            diff.image.write(`./test/reports/allure-results/winterPackage.png`);
+            diff.image.write(`./test/reports/allure-results/400-${namePage}.png`);
             // проверяем допустипость отличия в пикселях
-            expect(diff.percent).to.be.below(0.01);
+            expect(diff.percent).to.be.below(0.04);
             // проверем допустимость отличия в растоянии
             expect(distance).to.be.below(0.1);
         }
-      });
+    });
+
+    it('Сompare screenshots 800', async () => {
+        // делаем планшетный размер экрана
+        browser.windowHandleSize ({width: 800, height: 1200});
+        browser.pause(3000);
+        // берём скриншот с локала
+        ctx.originalScreenshot = `./snapshot/screenshotOption/800-${namePage}.png`;
+        // делаем текущий скриншот
+        ctx.newScreenshot = browser.screenshot().value;
+
+        expect(ctx.originalScreenshot).not.equal(null);
+        expect(ctx.newScreenshot).not.equal(null);
+    
+        const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+        const diff = await browser.helpers.compareScreenshotsDiff(ctx.originalScreenshot, ctx.newScreenshot, '0');
+        console.log(distance);
+        console.log(diff.percent);
+    
+        // expect(distance).to.be.above(0);
+        if(diff.percent > 0.04 || distance > 0.07) {
+            // если большое различие, то сохраняем изображение с отличием
+            diff.image.write(`./test/reports/allure-results/800-${namePage}.png`);
+            // проверяем допустипость отличия в пикселях
+            expect(diff.percent).to.be.below(0.04);
+            // проверем допустимость отличия в растоянии
+            expect(distance).to.be.below(0.1);
+        }
+    });
+
+    it('Сompare screenshots 1366', async () => {
+        // делаем планшетный размер экрана
+        browser.windowHandleSize ({width: 1366, height: 1200});
+        browser.pause(3000);
+        // берём скриншот с локала
+        ctx.originalScreenshot = `./snapshot/screenshotOption/1366-${namePage}.png`;
+        // делаем текущий скриншот
+        ctx.newScreenshot = browser.screenshot().value;
+
+        expect(ctx.originalScreenshot).not.equal(null);
+        expect(ctx.newScreenshot).not.equal(null);
+    
+        const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+        const diff = await browser.helpers.compareScreenshotsDiff(ctx.originalScreenshot, ctx.newScreenshot, '0');
+        console.log(distance);
+        console.log(diff.percent);
+    
+        // expect(distance).to.be.above(0);
+        if(diff.percent > 0.04 || distance > 0.07) {
+            // если большое различие, то сохраняем изображение с отличием
+            diff.image.write(`./test/reports/allure-results/1366-${namePage}.png`);
+            // проверяем допустипость отличия в пикселях
+            expect(diff.percent).to.be.below(0.04);
+            // проверем допустимость отличия в растоянии
+            expect(distance).to.be.below(0.07);
+        }
+    });
 });
