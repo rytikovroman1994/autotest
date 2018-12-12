db77ba6b5515ad371b3b2f0b27105dddcad1f20c
Kodix <Kodix>
2018-12-12 14:53:55 +0300

    Рефакторинг тестов Сидения

--- test/specs/NFZ/carsAvailable/filter/interior/sittings/skin.js
+++ test/specs/NFZ/carsAvailable/filter/interior/sittings/skin.js
@@ -1,10 +1,12 @@
+import NfzFilter from 'Pageobjects/nfz-filter.js'
+
 describe('test sittings scin', () => {
     // выносим часто используемое название условия комплектации
     let conditions = 'Кожа';
     before('open page filter', () => {
         browser.helpers.openFilter();
         // проверяем переход на страницу Интерьер
-        browser.click('.avn008_filter__tab[data-name="Интерьер"]');
+        NfzFilter.interior();
     }); 
 
     // проверяем работу чекбоскса
