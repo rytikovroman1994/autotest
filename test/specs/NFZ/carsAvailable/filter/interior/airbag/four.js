8c425e27acede958d8b70b640b08dbd10a03697b
Kodix <Kodix>
2018-12-12 14:48:50 +0300

    Рефакторинг тестов Подушки безопасности

--- test/specs/NFZ/carsAvailable/filter/interior/airbag/four.js
+++ test/specs/NFZ/carsAvailable/filter/interior/airbag/four.js
@@ -1,10 +1,12 @@
+import NfzFilter from 'Pageobjects/nfz-filter.js'
+
 describe('test airbag four', () => {
     // выносим часто используемое название условия комплектации
     let conditions = '4';
     before('open page filter', () => {
         browser.helpers.openFilter();
         // проверяем переход на страницу Интерьер
-        browser.click('.avn008_filter__tab[data-name="Интерьер"]');
+        NfzFilter.interior();
     }); 
 
     // проверяем работу чекбоскса
--- test/specs/NFZ/carsAvailable/filter/interior/airbag/one.js
+++ test/specs/NFZ/carsAvailable/filter/interior/airbag/one.js
@@ -1,10 +1,12 @@
+import NfzFilter from 'Pageobjects/nfz-filter.js'
+
 describe('test airbag one', () => {
     // выносим часто используемое название условия комплектации
     let conditions = '1';
     before('open page filter', () => {
         browser.helpers.openFilter();
         // проверяем переход на страницу Интерьер
-        browser.click('.avn008_filter__tab[data-name="Интерьер"]');
+        NfzFilter.interior();
     }); 
 
     // проверяем работу чекбоскса
