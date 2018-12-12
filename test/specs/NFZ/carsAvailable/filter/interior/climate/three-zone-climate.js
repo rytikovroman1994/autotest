41a38e1fb7755825c4b6cbca2a6fa09347ee24a6
Kodix <Kodix>
2018-12-12 14:49:01 +0300

    Рефакторинг тестов Климат

--- test/specs/NFZ/carsAvailable/filter/interior/climate/three-zone-climate.js
+++ test/specs/NFZ/carsAvailable/filter/interior/climate/three-zone-climate.js
@@ -1,10 +1,12 @@
+import NfzFilter from 'Pageobjects/nfz-filter.js'
+
 describe('test three zone climate', () => {
     // выносим часто используемое название условия комплектации
     let conditions = '3х зонный';
     before('open page filter', () => {
         browser.helpers.openFilter();
         // проверяем переход на страницу Интерьер
-        browser.click('.avn008_filter__tab[data-name="Интерьер"]');
+        NfzFilter.interior();
     }); 
 
     // проверяем работу чекбоскса
