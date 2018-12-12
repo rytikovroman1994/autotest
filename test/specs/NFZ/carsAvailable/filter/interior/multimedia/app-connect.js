c1ab6d0f6b560eae1c9d24cd2b1c02e0166aa800
Kodix <Kodix>
2018-12-12 15:02:18 +0300

    Рефакторинг тестов Мультимедиа

--- test/specs/NFZ/carsAvailable/filter/interior/multimedia/app-connect.js
+++ test/specs/NFZ/carsAvailable/filter/interior/multimedia/app-connect.js
@@ -1,10 +1,12 @@
+import NfzFilter from 'Pageobjects/nfz-filter.js'
+
 describe('test multimedia App Connect', () => {
     // выносим часто используемое название условия комплектации
     let conditions = 'App Connect';
     before('open page filter', () => {
         browser.helpers.openFilter();
         // проверяем переход на страницу Интерьер
-        browser.click('.avn008_filter__tab[data-name="Интерьер"]');
+        NfzFilter.interior();
     }); 
 
     // проверяем работу чекбоскса
