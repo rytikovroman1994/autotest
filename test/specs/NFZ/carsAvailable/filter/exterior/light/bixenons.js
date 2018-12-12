eba5b480aceb1cd547418819b7bcd63bbac23afc
Kodix <Kodix>
2018-12-12 14:00:22 +0300

    Рефакторинг тестов в категории Свет

--- test/specs/NFZ/carsAvailable/filter/exterior/light/bixenons.js
+++ test/specs/NFZ/carsAvailable/filter/exterior/light/bixenons.js
@@ -1,10 +1,12 @@
+import NfzFilter from 'Pageobjects/nfz-filter.js'
+
 describe('test light bixenon', () => {
     // выносим часто используемое название условия комплектации
     let conditions = 'Биксенон / Светодиоды';
     before('open page filter', () => {
         browser.helpers.openFilter();
         // проверяем переход на страницу Интерьер
-        browser.click('.avn008_filter__tab[data-name="Экстерьер"]');
+        NfzFilter.exterior();
     }); 
 
     // проверяем работу чекбоскса
