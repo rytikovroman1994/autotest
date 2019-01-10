f07aaf3dd285019e9f46d4b1480f5cb79106188d
Kodix <Kodix>
2019-01-10 12:56:30 +0300

    Логи алюра

--- test/specs/PKW/accessServer/status.js
+++ test/specs/PKW/accessServer/status.js
@@ -6,8 +6,9 @@ describe('status server', () => {
     it('check status server', () => {
         var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", 'https://vw.kodix.ru/', false);
-        xhr.send("result_cat=true");
+        xhr.open("GET", 'https://vw.kodix.ru/api/offers/cities', false);
+        const result = xhr.send("result_cat=true");
+        console.log(result);
 
         if (xhr.status != 200) {
             // обработать ошибку
