ea4565ed642e9303433513c124a15ba73555dc6c
Kodix <kodix@Kodixs-MacBook-Pro.local>
2019-01-23 17:09:26 +0300

    Пробный тест который не будет запускаться с остальынми

--- test/test-data/test-zapros.js
+++ test/test-data/test-zapros.js
@@ -1,15 +1,34 @@
+import "isomorphic-fetch"
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 describe('test test', () => {
     before('open site', () => {
         browser.url('https://nfz.kodix.ru/');
     });
 
     it('test test 2', () => {
-        var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
         var xhr = new XMLHttpRequest();
         xhr.open("GET", `https://nfz.kodix.ru/api/offers/filter`, false);
-        xhr.send('trimline":["amarok_1pa_comfortline"]');
-        // const text = JSON.parse(xhr.responseText);
-        // console.log(text.models);
-        console.log();
+        xhr.send();
+        const text = JSON.parse(xhr.responseText);
+        // console.log(text.models.amarok.kits);
+
+        // fetch("https://nfz.kodix.ru/api/offers/filter")
+        //     .then(r =>  r.json().then(Blob => ({status: r.status, body: Blob})))
+        //     .then(obj => console.log(obj));
+    });
+
+    it('test post', () => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", `https://nfz.kodix.ru/api/offers/filter`, false);
+        var options = {
+            filter: {
+                trimline: ["4-caddy-comfortline"]
+            }  
+        }
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.send(JSON.stringify(options));
+        console.log(xhr.status);
+        expect(xhr.status).to.be.equal(200);
     });
 });
