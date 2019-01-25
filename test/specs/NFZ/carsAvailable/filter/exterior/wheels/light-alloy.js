90aee9585cd05b63529c262b033eebb4f8616e5f
Kodix <rytikovroman1994@gmail.com>
2019-01-25 12:50:13 +0300

    Вынес проверку и повысил стабильность

--- test/specs/NFZ/carsAvailable/filter/exterior/wheels/light-alloy.js
+++ test/specs/NFZ/carsAvailable/filter/exterior/wheels/light-alloy.js
@@ -5,18 +5,24 @@ describe('test wheels light-alloy', () => {
     let conditions = 'Легкосплавные';
     before('open page filter', () => {
         browser.helpers.openFilter();
-        // проверяем переход на страницу Экстерьера
-        NfzFilter.exterior();
     }); 
 
+    it('Open page exterior', function() {
+        this.retries(3);
+        // проверяем переход на страницу Экстерьера
+        NfzFilter.exterior();
+    });
+
     // проверяем работу чекбоскса
-    it(`Check checkbox ${conditions}`, () => {
+    it(`Check checkbox ${conditions}`, function() {
+        this.retries(3);
         // проверяем работу чекбокса
        browser.helpers.checkCheckboxNfz(conditions, 'ЛЕГКОСПЛАВНЫЕ ДИСКИ', 'alloy-wheel');
     });
 
     // проверяем, что условие появилось в деталке машины
-    it('Check the equipment in detail', () => {
+    it('Check the equipment in detail', function() {
+        this.retries(3);
         const newArray = browser.helpers.checkConditionsNfz(conditions, 'Легкосплавные диски');
         // проверяем
         expect(newArray).to.be.equal('Легкосплавные диски');
