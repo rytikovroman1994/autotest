e6c18fa53a24ed28b34d6c45388eceda70a0783c
Kodix <rytikovroman1994@gmail.com>
2019-01-25 12:49:49 +0300

    Вынес проверку и повысил стабильность

--- test/specs/NFZ/carsAvailable/filter/exterior/wheels/steel.js
+++ test/specs/NFZ/carsAvailable/filter/exterior/wheels/steel.js
@@ -3,18 +3,24 @@ describe('test wheels steel', () => {
     let conditions = 'Стальные';
     before('open page filter', () => {
         browser.helpers.openFilter();
-        // проверяем переход на страницу Интерьер
-        browser.click('.avn008_filter__tab[data-name="Экстерьер"]');
     }); 
 
+    it('Open page exterior', function() {
+        this.retries(3);
+        // проверяем переход на страницу Интерьер
+        browser.click('.avn008_filter__tab[data-name="Экстерьер"]');
+    });
+
     // проверяем работу чекбоскса
-    it(`Check checkbox ${conditions}`, () => {
+    it(`Check checkbox ${conditions}`, function() {
+        this.retries(3);
         // проверяем работу чекбокса
-       browser.helpers.checkCheckboxNfz(conditions, 'СТАЛЬНЫЕ ДИСКИ');
+       browser.helpers.checkCheckboxNfz(conditions, 'СТАЛЬНЫЕ ДИСКИ','steel-wheel');
     });
 
     // проверяем, что условие появилось в деталке машины
-    it('Check the equipment in detail', () => {
+    it('Check the equipment in detail', function() {
+        this.retries(3);
         const newArray = browser.helpers.checkConditionsNfz(conditions, 'Стальные диски');
         // проверяем
         expect(newArray).to.be.equal('Стальные диски');
