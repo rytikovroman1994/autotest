b2f557df56207512eac2f0bc7b4fcac58ee28661
Kodix <rytikovroman1994@gmail.com>
2019-02-05 16:57:07 +0300

    Переделал тест на более стабильный

--- test/specs/PKW/carsAvailable/basicField/availability-kits.js
+++ test/specs/PKW/carsAvailable/basicField/availability-kits.js
@@ -1,11 +1,16 @@
 describe(' test availability of kits', () => {
+    // запоминаем имя браузера
+    let nameBrowser;
     before('open filter model', () => {
         browser.helpers.openSite();
+        // получаем имя браузера 
+        nameBrowser = browser.desiredCapabilities.browserName;
+        console.log(nameBrowser);
     });
 
     // проверяем наличие комплектаций и их активность
     it('check availability kits', function() {
-        this.retries(3);
+        if(nameBrowser === "chrome") {
         // получаем количетсво карточек
         const cards = $$('.slick-track .avn008_car').length;
         for(let i = 1; i <= cards; i++) {
@@ -21,12 +26,17 @@ describe(' test availability of kits', () => {
             const equipment = $$(`.slick-slider> div > div > div:nth-child(${i}) .icon-nextstep-checkmark`).length;
             // получаем имя модели 
             const nameModel = browser.getHTML(`.slick-initialized > div > div > div:nth-child(${i}) .avn008_car__title`, false);
+            // наводим фокус на карточку 
+            const test = $(`.slick-initialized > div > div > div:nth-child(${i}) > div > div`);
+            test.click();
 
             for(let y = 2; y <= equipment + 1; y++ ) {
                 // нажимаем на чекбокс комплектации
-                browser.execute(function(i,y) {
-                    document.querySelector(`.slick-initialized > div > div > div:nth-child(${i}) label:nth-child(${y}) > div`).click();
-                }, i, y);
+                browser.waitUntil(
+                    () => browser.isVisible(`.slick-initialized > div > div > div:nth-child(${i}) label:nth-child(${y}) > div`),
+                    10000, 'Чекбоксы в карточке не появились');
+                browser.click(`.slick-initialized > div > div > div:nth-child(${i}) label:nth-child(${y}) > div`);
+        
                 // получаем имя комплектации
                 const nameEquipment = browser.getHTML(`div:nth-child(${i}) > div > div > div > div:nth-child(3) > div > label:nth-child(${y}) .avn008_car__kit-title`, false);
                 // проверяем, что появилось условие в фильтре
@@ -40,9 +50,10 @@ describe(' test availability of kits', () => {
                 
                 browser.waitForVisible('.avn008_filter-value-item_image');
                 // убираем условие комплектации
-                browser.execute(function(i,y) {
-                    document.querySelector(`.slick-initialized > div > div > div:nth-child(${i}) label:nth-child(${y}) > div`).click();
-                }, i, y);
+                // browser.execute(function(i,y) {
+                //     document.querySelector(`.slick-initialized > div > div > div:nth-child(${i}) label:nth-child(${y}) > div`).click();
+                // }, i, y);
+                browser.click(`.slick-initialized > div > div > div:nth-child(${i}) label:nth-child(${y}) > div`);
 
                 // проверяем, что условие в фильтре пропало
                 browser.waitUntil(
@@ -50,5 +61,6 @@ describe(' test availability of kits', () => {
                     5000, "Выбранная комплектация не пропала из фильтра");
             }
         }
+    }
     }); 
 });
