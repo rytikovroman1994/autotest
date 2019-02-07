29f20436ed2e4cc0d8dff6305d11f06723366ca5
Kodix <rytikovroman1994@gmail.com>
2019-02-07 19:59:40 +0300

    Правка логики

--- helpers/PKW/more-detail.js
+++ helpers/PKW/more-detail.js
@@ -16,17 +16,18 @@ export default function moreDetail(conditions) {
         40000, `Картинка ${conditions} не отображается в Подробнее`);
     browser.waitForVisible('.avn015_content .image-container img');
     // проверяем что блоки не налазят друг на друга
-    const positionImg = browser.getLocation('.avn015_content .image-container img', 'y');
+    const positionImg = browser.getLocation('.avn015_content .image-container img');
     const sizeImg = browser.getElementSize('.avn015_content .image-container img','height');
 
-    const positionText = browser.getLocation('.avn015_content .avn015_description', 'y');
+    const positionText = browser.getLocation('.avn015_content .avn015_description');
     const sizeText = browser.getElementSize('.avn015_content .avn015_description', 'height');
 
-    const positionBottomText = browser.getLocation('.avn015_content .avn015_disclaimer', 'y');
+    const positionBottomText = browser.getLocation('.avn015_content .avn015_disclaimer');
     const sizeBottomText = browser.getElementSize('.avn015_content .avn015_disclaimer', 'height');
 
     browser.waitUntil(
-        () => (positionImg + sizeImg) < (positionText + sizeText) < (positionBottomText + sizeBottomText),
+        () => (positionImg.y + sizeImg) < (positionText.y) < 
+        (positionText.y + sizeText) < (positionBottomText.y) < (positionBottomText.y + sizeBottomText),
         5000, "Блоки в подробнее наскакивают друг на друга");
     // страховочная пауза
     browser.pause(2000);
