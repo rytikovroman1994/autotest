42d5cf2da1dd5a4c658d57997fafb7aa30a42edf
Kodix <rytikovroman1994@gmail.com>
2019-02-08 12:05:23 +0300

    Добавил репортеры

--- test/specs/PKW/carsAvailable/filter/interior/multimedia/premium-audio.js
+++ test/specs/PKW/carsAvailable/filter/interior/multimedia/premium-audio.js
@@ -17,6 +17,8 @@ describe('test premium audio', () => {
     let diff;
     before(' open page multimedia', () => {
         browser.helpers.openSite();
+        // получаем имя браузера 
+        nameBrowser = browser.desiredCapabilities.browserName;
     });
 
     // выносим проверку в отдельный тест
@@ -38,7 +40,7 @@ describe('test premium audio', () => {
         // открываем всплывающее окно подробнее и делаем скриншот
         browser.helpers.moreDetail(conditions);
         // берём скриншот с локала
-        ctx.originalScreenshot = 'snapshot/screenshotInterior/premiumAudio.png';
+        ctx.originalScreenshot = `snapshot/screenshotInterior/${nameBrowser}/premiumAudio.png`;
         // делаем актуальный скриншот
         ctx.newScreenshot = browser.screenshot().value;
      });
@@ -47,19 +49,28 @@ describe('test premium audio', () => {
         expect(ctx.originalScreenshot).not.equal(null);
         expect(ctx.newScreenshot).not.equal(null);
     
-        const distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
-        const diff = await browser.helpers.compareScreenshotsDiff(ctx.originalScreenshot, ctx.newScreenshot, '0');
-    
-        // expect(distance).to.be.above(0);
-        if(diff.percent > 0.01 || distance > 0.1) {
-            // если большое различие, то сохраняем изображение с отличием
-            diff.image.write(`./test/reports/allure-results/${conditions}.png`);
-            // проверяем допустипость отличия в пикселях
-            expect(diff.percent).to.be.below(0.01);
-            // проверем допустимость отличия в растоянии
-            expect(distance).to.be.below(0.1);
-        }
+        distance = await browser.helpers.compareScreenshots(ctx.originalScreenshot, ctx.newScreenshot);
+        diff = await browser.helpers.compareScreenshotsDiff(ctx.originalScreenshot, ctx.newScreenshot, '0');
       });
+
+      afterEach(function() {
+        if(this.currentTest.title === 'Compare screenshots'){
+            if(diff.percent > 0.04 || distance > 0.07) {
+                browser.call(()=> {
+                    return new Promise((resolve)=>{
+                        diff.image.getBuffer(Jimp.AUTO, (err, res) => {
+                        resolve(res);
+                        });
+                    })
+                    .then((res)=>reporter.createAttachment("difference", Buffer.from(res, "base64"), 'image/png'));
+                });
+            // проверяем допустипость отличия в пикселях
+            expect(diff.percent).to.be.below(0.04);
+            // проверем допустимость отличия в растоянии
+            expect(distance).to.be.below(0.07);
+            }
+        }
+    });
 
       // проверяем, что условие появилось в деталке машины
       it('Check the equipment in detail', () => {
